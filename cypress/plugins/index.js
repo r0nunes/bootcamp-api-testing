44da14dce15000ca262b99c8304a945de118cbb7
r0nunes <ronune.s@hotmail.com>
2020-11-18 10:13:46 -0300

    multiple tags

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,8 +15,24 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-const selectTestsWithGrep = require('cypress-select-tests/grep')
+
+
+const selectTests = require('cypress-select-tests')
+
+const pickTests = (filename, foundTests, cypressConfig) => {
+
+  let foundedTests = []
+
+  cypressConfig.env.grep.forEach(grepName => {
+   let testNames = foundTests.filter(fullTestName => fullTestName.join(' ').includes(grepName))
+    if (testNames) {
+      foundedTests.push(...testNames)
+    }
+  })
+
+  return foundedTests;
+}
 
 module.exports = (on, config) => {
-  on('file:preprocessor', selectTestsWithGrep(config))
+  on('file:preprocessor', selectTests(config, pickTests))
 }
